4faca3fe1e26ade4082107f52c6a8aeb7a8f3836
Dominika Sedileková <49202790+Dominika5505@users.noreply.github.com>
2019-10-10 20:43:17 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -5,3 +5,13 @@ hamburger.addEventListener('click', function() {
     ul.classList.toggle('toggle');
     hamburger.classList.toggle('toggle');
 });
+
+var element = document.documentElement;
+  
+if(element.scrollHeight > element.clientHeight) {
+  // Overflow detected; force scroll bar
+  element.style.overflow = 'scrollbar';
+} else {
+  // No overflow detected; prevent scroll bar
+  element.style.overflow = 'hidden';
+}
