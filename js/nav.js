4659e6bab392553adbd467978c064c5a279edbb0
Dominika Sedileková <49202790+Dominika5505@users.noreply.github.com>
2019-10-12 13:03:55 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
 const ul = document.querySelector('.main-nav');
 const hamburger = document.querySelector('.hamburger-wrap');
+const mainGalleryBanner = document.querySelectorAll('.main-gallery-banner')[0];
 
 hamburger.addEventListener('click', function() {
     ul.classList.toggle('toggle');
@@ -16,3 +17,18 @@ if(element.scrollHeight > element.clientHeight) {
   element.style.overflow = 'hidden';
 }
 
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    console.log(mainGalleryBanner);
+    mainGalleryBanner.classList.add('full-height');
+    mainGalleryBanner.style.background = 'none';
+  } else {
+    mainGalleryBanner.removeAttribute('data-parallax');
+    mainGalleryBanner.removeAttribute('data-image-src');
+    mainGalleryBanner.style.background = "url(../resources/activity-adventure-blur-297642.jpg)";
+  }
+}
+
+var x = window.matchMedia("(min-width: 992px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
